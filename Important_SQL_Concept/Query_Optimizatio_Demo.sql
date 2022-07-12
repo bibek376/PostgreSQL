2569458b3d94930a3100fd76633992a98e32c6fd
bibek376 <cu.18bcs6728@gmail.com>
2022-07-12 10:59:09 +0545

    Query Optimization Final

--- Important_SQL_Concept/Query_Optimizatio_Demo.sql
+++ Important_SQL_Concept/Query_Optimizatio_Demo.sql
@@ -149,6 +149,90 @@ explain select d.department_id ,d.department_name from department d
 where exists (select p.department_id from project p
 where d.department_id=p.department_id);
 
+----------------------------------------------------------------
+--Avoid operator
+
+explain SELECT e.employee_id , d.department_name
+FROM employee e join department d
+on e.department_id =d.department_id
+where salary >= 3000 and salary<= 5000 ;
+
+explain SELECT e.employee_id , e.first_name
+FROM employee e join department d
+on e.department_id =d.department_id
+WHERE salary BETWEEN 3000 and 5000;
+
+--------------------------------------------------------------------
+--Combine multiples scans with case statements
+
+explain
+select e.department_id ,
+(select count(e.employee_id) from employee e where salary<1100),
+(select count(e.employee_id) from employee e where salary between
+1100 and 3000),
+(select count(e.employee_id) from employee e where salary>3000)
+from employee e
+group by e.department_id ;
+
+
+
+explain select e.department_id ,count( case when salary<1100 then 1 else null end ) count1,
+count(case when salary between 1100 and 3000 then 1 else null end) count2,
+count( case when salary>3000 then 1 else null end) count3
+from employee e
+group by e.department_id ;
+
+--------------------------------------------------------------------------
+--Making joins less complicated
+
+explain
+SELECT d.department_name,
+count(employee_id) totalemp,
+SUM(salary) AS salary ,
+count(p.project_id) totalproject
+from employee e join department d
+on e.department_id =d.department_id
+join project p on p.department_id =d.department_id
+group by d.department_name;
+
+
+explain
+SELECT d.department_name ,a.totalemp,a.salary,p.totalproject
+FROM department d
+INNER JOIN (
+SELECT department_id,
+count(employee_id) totalemp,
+SUM(salary) AS salary
+FROM employee GROUP BY department_id
+) a
+ON d.department_id = a.department_id
+join (select department_id,
+count(p.project_id) totalproject
+from project p
+group by department_id ) p onp.department_id=d.department_id ;
+
+
+---------------------------------------------------------------------
+--Wrong Join Case gives wrong results
+
+explain select d.department_name , count(e.employee_id), count(p.project_id)
+from department d join employee e
+on e.department_id = d.department_id
+join project p on p.department_id =d.department_id
+group by d.department_name;
+
+
+select d.department_name , ee.ecount, pp.pcount
+from department d
+left join (select e.department_id,count(e.employee_id) ecount
+from employee e
+group by e.department_id ) ee on d.department_id =
+ee.department_id
+left join (select p2.department_id,count(p2.project_id) pcount
+from project p2
+group by p2.department_id ) pp
+on pp.department_id=ee.department_id;
+
 
 
 
